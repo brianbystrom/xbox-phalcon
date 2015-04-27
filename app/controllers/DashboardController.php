8a89079ddee27b12014e8a839667f0887201bfaf
Brian Bystrom <brianbystrom@gmail.com>
2015-04-27 16:47:20 -0400

    Messing around with queries.

--- app/controllers/DashboardController.php
+++ app/controllers/DashboardController.php
@@ -1,6 +1,7 @@
 <?php
 
 use \Phalcon\Tag;
+use \Phalcon\Mvc\Model\Query;
 
 class DashboardController extends BaseController
 {
@@ -10,11 +11,23 @@ class DashboardController extends BaseController
 		Tag::setTitle('Dashboard');
 		parent::initialize();
 
-		$manager = Manager::findFirstByClientId(23);
+		$query = $this->modelsManager->createQuery('SELECT * FROM survey');
+		$surveys = $query->execute();
+
+		foreach($surveys as $survey) {
+			echo $survey->id.' / '.$survey->score.' / '.$survey->client_id.'<br>';
+		}
+
+
+		echo '<hr>';
+
+
+		$manager = Manager::findFirstByClientId(648);
 
 		foreach ($manager->agent as $agent) {
 			foreach ($agent->survey as $survey) {
-				echo $survey->client_id.' / '.$survey->score.' / '.$survey->driver.'<br>';
+				//$surveys[] = $survey->id;
+				echo $survey->id.' / '.$survey->score.' / '.$survey->client_id.'<br>';
 			}
 		}
 	}
